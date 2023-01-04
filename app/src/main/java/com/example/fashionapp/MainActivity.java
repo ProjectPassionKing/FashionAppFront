d1bdd32916f4ac11306a2aa251ea66df753f5215
unknown <soojlee0106@naver.com>
2023-01-04 14:47:52 +0900

    Change menu to hamburger

--- app/src/main/java/com/example/fashionapp/MainActivity.java
+++ app/src/main/java/com/example/fashionapp/MainActivity.java
@@ -36,7 +36,7 @@ public class MainActivity extends AppCompatActivity {
         appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
 
-        binding.fab.setOnClickListener(new View.OnClickListener() {
+        binding.toolbar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
