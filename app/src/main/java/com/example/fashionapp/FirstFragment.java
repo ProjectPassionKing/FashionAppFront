7f25cd98c839ba5052b3b8d015686b505b203540
soojlee0106 <soojlee0106@naver.com>
2023-01-13 17:00:30 +0900

    홈화면 애니메이션 효과 추가

--- app/src/main/java/com/example/fashionapp/FirstFragment.java
+++ app/src/main/java/com/example/fashionapp/FirstFragment.java
@@ -1,9 +1,13 @@
 package com.example.fashionapp;
 
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -22,9 +26,32 @@ public class FirstFragment extends Fragment {
     ) {
 
         binding = FragmentFirstBinding.inflate(inflater, container, false);
+
+        VideoView videoView = binding.getRoot().findViewById(R.id.avatar_video);
+        Uri videoUri = Uri.parse("android.resource://" + getActivity().getPackageName() + "/" + R.raw.avatar_video);
+        videoView.setVideoURI(videoUri);
+        MediaController mediaController = new MediaController(getActivity());
+        videoView.setMediaController(mediaController);
+        videoView.start();
+
+        final int[] cnt = {0};
+        videoView.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                videoView.start();
+                cnt[0]++;
+                if (cnt[0]==3){
+                    videoView.stopPlayback();
+                    NavHostFragment.findNavController(FirstFragment.this)
+                            .navigate(R.id.action_FirstFragment_to_MainFragment);
+                }
+            }
+        });
+
         return binding.getRoot();
 
     }
+
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
