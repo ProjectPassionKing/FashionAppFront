e1c1ea6b60a4af7e1ca0d794cd5136a6e51423b6
Soo <soojlee0106@naver.com>
2023-01-13 17:05:20 +0900

    홈화면에 애니메이션 효과 구현 (#15)
    * working on filter
    
    * 홈화면 애니메이션 효과 추가

--- app/src/main/java/com/example/fashionapp/ResultFragment.java
+++ app/src/main/java/com/example/fashionapp/ResultFragment.java
@@ -1,21 +1,44 @@
 package com.example.fashionapp;
 
+import static android.app.Activity.RESULT_OK;
+import static android.content.ContentValues.TAG;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 import com.example.fashionapp.databinding.FragmentResultBinding;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class ResultFragment extends Fragment {
 
     private MediaPlayer mediaPlayer;
     private FragmentResultBinding binding;
     public static String result;
+
+    Button camera_open_id;
+
+    ActivityResultLauncher<Intent> activityResultLauncher;
 
     @Override
     public View onCreateView(
@@ -38,6 +61,43 @@ public class ResultFragment extends Fragment {
         mediaPlayer = MediaPlayer.create(this.getContext(), R.raw.straightresult);
         playSound();
 
+        camera_open_id = getView().findViewById(R.id.photo_simulation_button);
+
+        activityResultLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback<ActivityResult>() {
+            @Override
+            public void onActivityResult(ActivityResult result) {
+                if (result.getResultCode() == RESULT_OK && result.getData() != null) {
+                    Bitmap photo = (Bitmap) result.getData().getExtras().get("data");
+
+                    String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+                    String imageFileName = "JPEG_" + timeStamp + "_";
+                    File storageDir = getActivity().getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+                    File image = null;
+                    try {
+                        image = File.createTempFile(
+                                imageFileName,  /* prefix */
+                                ".jpg",         /* suffix */
+                                storageDir      /* directory */
+                        );
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+
+                    try {
+                        FileOutputStream fos = new FileOutputStream(image);
+                        photo.compress(Bitmap.CompressFormat.JPEG, 100, fos);
+                        fos.close();
+                    } catch (IOException e) {
+                        Log.e(TAG, "Error saving image file: " + e.getMessage());
+                    }
+
+                    NavHostFragment.findNavController(ResultFragment.this)
+                            .navigate(R.id.action_ResultFragment_to_SimulateFragment);
+                }
+            }
+
+        });
+
         MoreHorizontalScrollView moreScrollView = new MoreHorizontalScrollView(this);
         binding.scrollview.addView(moreScrollView);
 
@@ -54,6 +114,13 @@ public class ResultFragment extends Fragment {
             public void onClick(View view) {
                 NavHostFragment.findNavController(ResultFragment.this)
                         .navigate(R.id.action_ResultFragment_to_AllinOneFragment);
+            }
+        });
+        binding.photoSimulationButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent camera_intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                activityResultLauncher.launch(camera_intent);
             }
         });
     }
