366c9ea56aa0be066cba9905683f29a26a48696c
soojlee0106 <soojlee0106@naver.com>
2023-01-16 12:04:20 +0900

    진단 페이지에 애니메이션 추가

--- app/src/main/java/com/example/fashionapp/ResultFragment.java
+++ app/src/main/java/com/example/fashionapp/ResultFragment.java
@@ -6,6 +6,7 @@ import static android.content.ContentValues.TAG;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
@@ -14,6 +15,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
 import androidx.activity.result.ActivityResult;
 import androidx.activity.result.ActivityResultCallback;
@@ -22,6 +26,8 @@ import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
+
+import com.bumptech.glide.Glide;
 import com.example.fashionapp.databinding.FragmentResultBinding;
 
 import java.io.File;
@@ -47,6 +53,10 @@ public class ResultFragment extends Fragment {
     ) {
 
         binding = FragmentResultBinding.inflate(inflater, container, false);
+
+        ImageView gifImageView = binding.getRoot().findViewById(R.id.diagnose_video);
+        Glide.with(this).asGif().load(R.raw.diagnose_video).into(gifImageView);
+
         binding.questionTxtview.setText(result);
         return binding.getRoot();
 
