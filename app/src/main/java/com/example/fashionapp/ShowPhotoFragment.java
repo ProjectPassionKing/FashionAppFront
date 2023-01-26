a7fd11e2c8a7ded5b8469ce00b3c8eb4d9dcc4d6
soojlee0106 <soojlee0106@naver.com>
2023-01-26 16:04:26 +0900

    API 구현

--- app/src/main/java/com/example/fashionapp/ShowPhotoFragment.java
+++ app/src/main/java/com/example/fashionapp/ShowPhotoFragment.java
@@ -2,17 +2,24 @@ package com.example.fashionapp;
 
 import static android.app.Activity.RESULT_OK;
 
+import static org.apache.commons.io.FileUtils.readFileToByteArray;
+
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.os.Environment;
+import android.os.FileUtils;
 import android.provider.MediaStore;
+import android.util.Base64;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.result.ActivityResult;
 import androidx.activity.result.ActivityResultCallback;
@@ -22,15 +29,30 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
 import com.example.fashionapp.databinding.FragmentShowPhotoBinding;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ShowPhotoFragment extends Fragment {
 
     private FragmentShowPhotoBinding binding;
     Button camera_open_id;
     ImageView click_image_id;
+    TextView prediction;
+    String url = "http://127.0.0.1:5000/prediction";
 
     ActivityResultLauncher<Intent> activityResultLauncher;
 
@@ -39,7 +61,6 @@ public class ShowPhotoFragment extends Fragment {
             LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState
     ) {
-
         binding = FragmentShowPhotoBinding.inflate(inflater, container, false);
         return binding.getRoot();
 
@@ -75,6 +96,47 @@ public class ShowPhotoFragment extends Fragment {
                 }
             }
         });
+
+        prediction = getView().findViewById(R.id.prediction);
+
+        File finalMostRecentFile = mostRecentFile;
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, url,
+                new Response.Listener() {
+                    @Override
+                    public void onResponse(Object response) {
+                        try {
+                            JSONObject jsonObject = new JSONObject((String) response);
+                            String data = jsonObject.getString("prediction");
+                            prediction.setText(data);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(getContext(), error.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                }){
+            @Override
+            protected Map getParams() {
+
+                String base64Image = null;
+                try {
+                    base64Image = Base64.encodeToString(readFileToByteArray(finalMostRecentFile), Base64.DEFAULT);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                Map params = new HashMap();
+                params.put("file", base64Image);
+                return params;
+
+            }
+        };
+        RequestQueue queue = Volley.newRequestQueue(getContext());
+        queue.add(stringRequest);
 
         binding.homeBtn.setOnClickListener(new View.OnClickListener() {
             @Override
