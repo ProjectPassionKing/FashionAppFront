63acbbc1c0868c7a9c530fa305c14cbd6d2f335b
soojlee0106 <soojlee0106@naver.com>
2023-01-26 09:11:30 +0900

    마이크 기능 구현

--- app/src/main/java/com/example/fashionapp/MainFragment.java
+++ app/src/main/java/com/example/fashionapp/MainFragment.java
@@ -1,22 +1,30 @@
 package com.example.fashionapp;
 
 import android.media.MediaPlayer;
+import android.media.MediaRecorder;
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 import com.example.fashionapp.databinding.FragmentMainBinding;
 
+import java.io.File;
 import java.io.IOException;
 
 
 public class MainFragment extends Fragment {
     private MediaPlayer mediaPlayer;
     private FragmentMainBinding binding;
+
+    private MediaRecorder recorder;
+    private ImageButton recordButton;
 
     //context nullable처리 나중에
 //    public Context context1;
@@ -33,7 +41,6 @@ public class MainFragment extends Fragment {
             Bundle savedInstanceState
 
     ) {
-
         binding = FragmentMainBinding.inflate(inflater, container, false);
         return binding.getRoot();
 
@@ -45,8 +52,10 @@ public class MainFragment extends Fragment {
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
         mediaPlayer = MediaPlayer.create(this.getContext(), R.raw.morning);
         playSound();
+
         binding.homeBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -71,6 +80,43 @@ public class MainFragment extends Fragment {
                         .navigate(R.id.action_MainFragment_to_WardrobeFragment);
             }
         });
+
+        recordButton = getView().findViewById(R.id.mic_btn);
+        binding.micBtn.setOnTouchListener(new View.OnTouchListener()  {
+            @Override
+            public boolean onTouch(View view, MotionEvent event) {
+                if(event.getAction() == MotionEvent.ACTION_UP){
+                    startRecording();
+                    if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                        stopRecording();
+                    }
+                }
+                return false;
+            }
+        });
+    }
+
+    private void startRecording(){
+        recorder = new MediaRecorder();
+        recorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+        recorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+        recorder.setOutputFile(Environment.getExternalStorageDirectory() + File.separator
+                + Environment.DIRECTORY_DCIM + File.separator + "/audio.3gp");
+        recorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+
+        try {
+            recorder.prepare();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        recorder.start();
+    }
+
+    private void stopRecording(){
+        recorder.stop();
+        recorder.release();
+        recorder = null;
     }
 
     @Override
