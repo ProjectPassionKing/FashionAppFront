d20bef2c5aaa8bbae1a8f59806914cae54cd8335
soojlee0106 <soojlee0106@naver.com>
2023-01-31 19:24:03 +0900

    simulation 코드

---
+++ app/src/main/java/com/example/fashionapp/ShowSimFragment.java
@@ -0,0 +1,160 @@
+package com.example.fashionapp;
+
+import static android.app.Activity.RESULT_OK;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Base64;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
+
+import com.example.fashionapp.databinding.FragmentShowPhotoBinding;
+
+import java.io.File;
+import java.io.IOException;
+
+import okhttp3.MultipartBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
+public class ShowSimFragment extends Fragment {
+
+    private FragmentShowPhotoBinding binding;
+    Button camera_open_id;
+    ImageView click_image_id;
+    TextView prediction;
+
+    ActivityResultLauncher<Intent> activityResultLauncher;
+
+    @Override
+    public View onCreateView(
+            LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState
+    ) {
+        binding = FragmentShowPhotoBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+
+    }
+
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        File mostRecentFile = null;
+        long mostRecentModified = Long.MIN_VALUE;
+
+        File storageDir = getActivity().getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+
+        for (File file : storageDir.listFiles()) {
+            if (file.isFile() && file.lastModified() > mostRecentModified) {
+                mostRecentFile = file;
+                mostRecentModified = file.lastModified();
+            }
+        }
+
+        Bitmap bitmap = BitmapFactory.decodeFile(mostRecentFile.getAbsolutePath());
+        click_image_id = getView().findViewById(R.id.click_image);
+        click_image_id.setImageBitmap(bitmap);
+
+        camera_open_id = getView().findViewById(R.id.camera_button);
+
+        activityResultLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback<ActivityResult>() {
+            @Override
+            public void onActivityResult(ActivityResult result) {
+                if (result.getResultCode() == RESULT_OK && result.getData() != null) {
+                    Bitmap photo = (Bitmap) result.getData().getExtras().get("data");
+                    click_image_id.setImageBitmap(photo);
+                }
+            }
+        });
+
+        prediction = getView().findViewById(R.id.prediction);
+
+        File finalMostRecentFile = mostRecentFile;
+
+        new Thread(() ->
+        {
+            OkHttpClient client = new OkHttpClient();
+
+            RequestBody requestBody = new MultipartBody.Builder()
+                    .setType(MultipartBody.FORM)
+                    .addFormDataPart("file","file.jpg", RequestBody.create(finalMostRecentFile, MultipartBody.FORM))
+                    .build();
+
+            Request request = new Request.Builder()
+                    .url("http://172.23.247.89:5000/sim")
+                    .post(requestBody)
+                    .build();
+
+            try {
+                Response response = client.newCall(request).execute();
+                String responseBodyString = response.body().string();
+                System.out.println(responseBodyString);
+
+                byte[] decodedString = Base64.decode(responseBodyString, Base64.DEFAULT);
+                Bitmap decodedByte = BitmapFactory.decodeByteArray(decodedString, 0, decodedString.length);
+
+                getActivity().runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        click_image_id.setImageBitmap(decodedByte);
+                    }
+                });
+
+                response.close();
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }).start();
+
+
+        binding.homeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NavHostFragment.findNavController(ShowSimFragment.this)
+                        .navigate(R.id.action_ShowPhotoFragment_to_MainFragment);
+            }
+        });
+        binding.menuHamburger.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NavHostFragment.findNavController(ShowSimFragment.this)
+                        .navigate(R.id.action_ShowPhotoFragment_to_AllinOneFragment);
+            }
+        });
+        binding.cameraButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent camera_intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                activityResultLauncher.launch(camera_intent);
+            }
+        });
+    }
+
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+
+}
