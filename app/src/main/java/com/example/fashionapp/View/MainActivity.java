94b4967824067c69fd8e2f3a3810f50c752bb0f3
soojlee0106 <soojlee0106@naver.com>
2023-02-13 15:01:21 +0900

    Move hamburgerbar location

--- app/src/main/java/com/example/fashionapp/View/MainActivity.java
+++ app/src/main/java/com/example/fashionapp/View/MainActivity.java
@@ -47,13 +47,6 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
@@ -61,9 +54,6 @@ public class MainActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
 
         return super.onOptionsItemSelected(item);
     }
