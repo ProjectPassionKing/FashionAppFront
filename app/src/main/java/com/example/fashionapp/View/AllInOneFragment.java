4864234c903be9abfee9a78177de82c827a39054
Soo <soojlee0106@naver.com>
2023-02-15 10:19:17 +0900

    Dropdown 추가 (#44)
    * Add dropdowns
    
    * Resolve conflict
    
    * working on dropdown ui
    
    * spinner ui 완성

--- app/src/main/java/com/example/fashionapp/View/AllInOneFragment.java
+++ app/src/main/java/com/example/fashionapp/View/AllInOneFragment.java
@@ -4,6 +4,10 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
@@ -23,6 +27,54 @@ public class AllInOneFragment extends Fragment {
     ) {
 
         binding = FragmentAllInOneBinding.inflate(inflater, container, false);
+
+        Spinner spinner1 = binding.getRoot().findViewById(R.id.spinner1);
+        ArrayAdapter<String> adapter1 = new ArrayAdapter<>(binding.getRoot().getContext(), R.layout.spinner_item, new String[]{"         체형 진단","         체형 진단 진행 / 결과 보기", "         코디 팁"});
+        adapter1.setDropDownViewResource(R.layout.spinner_dropdown);
+        spinner1.setAdapter(adapter1);
+
+        spinner1.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                if (position == 1) {
+                    NavHostFragment.findNavController(AllInOneFragment.this)
+                            .navigate(R.id.action_global_DiagnosisSelectFragment);
+                }
+
+                if (position == 2) {
+                    NavHostFragment.findNavController(AllInOneFragment.this)
+                            .navigate(R.id.action_global_CordiTipFragment);
+                }
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {}
+        });
+
+        Spinner spinner2 = binding.getRoot().findViewById(R.id.spinner2);
+        ArrayAdapter<String> adapter2 = new ArrayAdapter<>(binding.getRoot().getContext(), R.layout.spinner_item, new String[]{"           옷 추천","           내가 입은 옷과 비슷한 옷", "           가상으로 옷 입어보기"});
+        adapter2.setDropDownViewResource(R.layout.spinner_dropdown);
+        spinner2.setAdapter(adapter2);
+
+        spinner2.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                if (position == 1) {
+                    NavHostFragment.findNavController(AllInOneFragment.this)
+                            .navigate(R.id.action_global_TakePhotoFragment);
+                }
+
+                if (position == 2) {
+                    NavHostFragment.findNavController(AllInOneFragment.this)
+                            .navigate(R.id.action_global_TakeSimFragment);
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {}
+        });
+
         return binding.getRoot();
 
     }
@@ -40,20 +92,6 @@ public class AllInOneFragment extends Fragment {
             public void onClick(View view) {
                 NavHostFragment.findNavController(AllInOneFragment.this)
                         .navigate(R.id.action_global_toHome);
-            }
-        });
-        binding.buttonEleventh.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                NavHostFragment.findNavController(AllInOneFragment.this)
-                        .navigate(R.id.action_global_DiagnosisSelectFragment);
-            }
-        });
-        binding.buttonTwelfth.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                NavHostFragment.findNavController(AllInOneFragment.this)
-                        .navigate(R.id.action_global_RecommandFragment);
             }
         });
     }
