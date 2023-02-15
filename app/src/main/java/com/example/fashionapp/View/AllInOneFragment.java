27fad4f2e989875d7170dc9213f3c73cc4ddf30b
soojlee0106 <soojlee0106@naver.com>
2023-02-15 09:21:59 +0900

    working on dropdown ui

--- app/src/main/java/com/example/fashionapp/View/AllInOneFragment.java
+++ app/src/main/java/com/example/fashionapp/View/AllInOneFragment.java
@@ -29,39 +29,50 @@ public class AllInOneFragment extends Fragment {
         binding = FragmentAllInOneBinding.inflate(inflater, container, false);
 
         Spinner spinner1 = binding.getRoot().findViewById(R.id.spinner1);
-        ArrayAdapter<String> adapter1 = new ArrayAdapter<>(binding.getRoot().getContext(), android.R.layout.simple_spinner_item, new String[]{"체형 진단 진행 / 결과 보기", "유형 자세히", "코디 팁"});
-        adapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        ArrayAdapter<String> adapter1 = new ArrayAdapter<>(binding.getRoot().getContext(), R.layout.spinner_item, new String[]{"","체형 진단 진행 / 결과 보기", "코디 팁"});
+        adapter1.setDropDownViewResource(R.layout.spinner_dropdown);
         spinner1.setAdapter(adapter1);
 
         spinner1.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                String selectedItem = (String) parent.getItemAtPosition(position);
-                // Do something with the selected item
+                if (position == 1) {
+                    NavHostFragment.findNavController(AllInOneFragment.this)
+                            .navigate(R.id.action_global_DiagnosisSelectFragment);
+                }
+
+                if (position == 2) {
+                    NavHostFragment.findNavController(AllInOneFragment.this)
+                            .navigate(R.id.action_global_CordiTipFragment);
+                }
+
             }
 
             @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-                // Do something when nothing is selected
-            }
+            public void onNothingSelected(AdapterView<?> parent) {}
         });
 
         Spinner spinner2 = binding.getRoot().findViewById(R.id.spinner2);
-        ArrayAdapter<String> adapter2 = new ArrayAdapter<>(binding.getRoot().getContext(), android.R.layout.simple_spinner_item, new String[]{"내가 입은 옷과 비슷한 옷", "체형에 어울리는 옷"});
+        ArrayAdapter<String> adapter2 = new ArrayAdapter<>(binding.getRoot().getContext(), android.R.layout.simple_spinner_item, new String[]{"","내가 입은 옷과 비슷한 옷", "가상으로 옷 입어보기"});
         adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner2.setAdapter(adapter2);
 
         spinner2.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                String selectedItem = (String) parent.getItemAtPosition(position);
-                // Do something with the selected item
+                if (position == 1) {
+                    NavHostFragment.findNavController(AllInOneFragment.this)
+                            .navigate(R.id.action_global_TakePhotoFragment);
+                }
+
+                if (position == 2) {
+                    NavHostFragment.findNavController(AllInOneFragment.this)
+                            .navigate(R.id.action_global_TakeSimFragment);
+                }
             }
 
             @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-                // Do something when nothing is selected
-            }
+            public void onNothingSelected(AdapterView<?> parent) {}
         });
 
         return binding.getRoot();
@@ -81,20 +92,6 @@ public class AllInOneFragment extends Fragment {
             public void onClick(View view) {
                 NavHostFragment.findNavController(AllInOneFragment.this)
                         .navigate(R.id.action_global_toHome);
-            }
-        });
-        binding.buttonEleventh.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                NavHostFragment.findNavController(AllInOneFragment.this)
-                        .navigate(R.id.action_global_DiagnosisSelectFragment);
-            }
-        });
-        binding.buttonTwelfth.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                NavHostFragment.findNavController(AllInOneFragment.this)
-                        .navigate(R.id.action_global_RecommandFragment);
             }
         });
     }
