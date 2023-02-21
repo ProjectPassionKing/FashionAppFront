f893e1764260e58686ccf2c29a98274f35dec5d9
하지현 <kkim.haji@gmail.com>
2023-02-21 11:01:49 +0900

    버튼 하나만 선택되도록 변경

--- app/src/main/java/com/example/fashionapp/View/LookbookFragment.java
+++ app/src/main/java/com/example/fashionapp/View/LookbookFragment.java
@@ -4,24 +4,25 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
-
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.fragment.NavHostFragment;
-
 import com.example.fashionapp.R;
 import com.example.fashionapp.ViewModel.SharedViewModel;
 import com.example.fashionapp.databinding.FragmentLookbookBinding;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class LookbookFragment extends Fragment {
 
     private FragmentLookbookBinding binding;
     ImageView box_image;
     SharedViewModel sharedViewModel;
-    boolean selected;
 
     @Override
     public View onCreateView(
@@ -40,52 +41,25 @@ public class LookbookFragment extends Fragment {
 
         sharedViewModel = new ViewModelProvider(requireActivity()).get(SharedViewModel.class);
         box_image = getView().findViewById(R.id.box_image);
-        selected = false;
 
-        binding.guest.setOnClickListener(new View.OnClickListener() {
+        View.OnClickListener onClickListener = new Button.OnClickListener(){
             @Override
-            public void onClick(View view) {
-                // Add change button
-                binding.guest.setSelected(!selected);
-                selected = !selected;
+            public void onClick(View v) {
+                binding.guest.setSelected(false);
+                binding.casual.setSelected(false);
+                binding.date.setSelected(false);
+                binding.sports.setSelected(false);
+                binding.work.setSelected(false);
+                v.setSelected(true);
             }
-        });
+        };
 
-        binding.casual.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // Add change button
-                binding.casual.setSelected(!selected);
-                selected = !selected;
-            }
-        });
+        binding.guest.setOnClickListener(onClickListener);
+        binding.casual.setOnClickListener(onClickListener);
+        binding.date.setOnClickListener(onClickListener);
+        binding.sports.setOnClickListener(onClickListener);
+        binding.work.setOnClickListener(onClickListener);
 
-        binding.date.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // Add change button
-                binding.date.setSelected(!selected);
-                selected = !selected;
-            }
-        });
-
-        binding.sports.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // Add change button
-                binding.sports.setSelected(!selected);
-                selected = !selected;
-            }
-        });
-
-        binding.work.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // Add change button
-                binding.work.setSelected(!selected);
-                selected = !selected;
-            }
-        });
 
         binding.homeBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -94,7 +68,6 @@ public class LookbookFragment extends Fragment {
                         .navigate(R.id.action_global_toHome);}
         });
     }
-
 
     @Override
     public void onDestroyView() {
