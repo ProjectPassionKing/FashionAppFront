74a7dfb80e8d8a731a6ddfd571995828ec747517
하지현 <kkim.haji@gmail.com>
2023-02-22 10:10:15 +0900

    [feat] 사진 클릭 시 팝업

--- app/src/main/java/com/example/fashionapp/View/LookbookFragment.java
+++ app/src/main/java/com/example/fashionapp/View/LookbookFragment.java
@@ -1,6 +1,7 @@
 package com.example.fashionapp.View;
 
 import android.annotation.SuppressLint;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,6 +11,7 @@ import android.widget.ImageButton;
 import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
@@ -26,7 +28,8 @@ public class LookbookFragment extends Fragment {
 
     private FragmentLookbookBinding binding;
     ImageView box_image;
-    SharedViewModel sharedViewModel;
+    private SharedViewModel sharedViewModel;
+    private AlertDialog dialog;
 
     @Override
     public View onCreateView(
@@ -64,12 +67,20 @@ public class LookbookFragment extends Fragment {
 
                 String btnId = getResources().getResourceName(v.getId()).split("/")[1];
 
+                String btnTxt = ((Button) v).getText().toString();
+
                 for (int i = 1; i < 10; i++) {
                     int id = getResources().getIdentifier("date_"+i, "id", getActivity().getPackageName());
                     ImageButton imgbtn = view.findViewById(id);
-                    imgbtn.setImageResource(getImageById(btnId, String.valueOf(i)));
+                    int imgid = getImageById(btnId, String.valueOf(i));
+                    imgbtn.setImageResource(imgid);
+                    imgbtn.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View imgv) {
+                            popUpImg(imgid, btnTxt);
+                        }
+                    });
                 }
-
                 v.setSelected(true);
             }
         };
@@ -87,6 +98,24 @@ public class LookbookFragment extends Fragment {
                         .navigate(R.id.action_global_toHome);
             }
         });
+    }
+
+    private void popUpImg(int imgId, String lookname){
+        ImageView imgview=new ImageView(getContext());
+        imgview.setImageResource(imgId);
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+        builder.setTitle(lookname+" 코디 추천");
+        builder.setView(imgview);
+
+        builder.setNegativeButton("종료", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                if (dialog!= null) dialog.dismiss();
+            }
+        });
+        dialog = builder.create();
+        dialog.show();
     }
 
     @Override
